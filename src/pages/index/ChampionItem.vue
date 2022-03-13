6e61515b20f050d81d2d63a91157203f3481b287
mekane <4631242+mekane84@users.noreply.github.com>
2022-03-12 20:29:55 -0600

    * rounded image borders

--- src/pages/index/ChampionItem.vue
+++ src/pages/index/ChampionItem.vue
@@ -1,7 +1,8 @@
 <template>
   <figure>
     <img :src="'https://mekane84.github.io/319-boys-assets/' + champion.image"
-         :alt="'Picture of ' + champion.champion + ' owner ' + champion.owner">
+         :alt="'Picture of ' + champion.champion + ' owner ' + champion.owner"
+         class="rounded">
     <figcaption class="lg:text-xl"> {{champion.year}} Champion &ndash; {{champion.champion}}, {{champion.owner}}  </figcaption>
   </figure>
 </template>
